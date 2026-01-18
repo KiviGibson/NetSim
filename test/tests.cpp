f8cdb11a7e635feb6edc7c7a633b6206023eb6ea
KiviGibson <poplawski143@gmail.com>
2026-01-19 00:04:40 +0100

    uncomment test

--- test/tests.cpp
+++ test/tests.cpp
@@ -64,7 +64,7 @@ TEST(PackageQueueTest, IsLifoCorrect) {
     EXPECT_EQ(p.get_id(), 1);
 }
 
-/*
+
 TEST(FactoryIOTest, ParseRamp) {
     std::istringstream iss("LOADING_RAMP id=1 delivery-interval=3");
     auto factory = load_factory_structure(iss);
@@ -74,7 +74,7 @@ TEST(FactoryIOTest, ParseRamp) {
     EXPECT_EQ(1, r.get_id());
     EXPECT_EQ(3, r.get_delivery_interval());
 }
-*/
+
 TEST(WorkerTest, HasBuffer) {
 
     Worker w(1, 2, std::make_unique<PackageQueue>(PackageQueueType::FIFO));
